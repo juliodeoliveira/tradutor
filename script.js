c4d61e2429af686b9745d4645a3d5053fc24d7c1
Júlio de Oliviera <96733150+juliodeoliveira@users.noreply.github.com>
2024-03-19 20:17:50 -0300

    Created

---
+++ script.js
@@ -0,0 +1,23 @@
+function verifyWord() {
+    let userInput = document.querySelector("#texto-esquerda");
+    let userResult = document.querySelector("#texto-direita");
+
+    if (userInput.value == "Júlio") {
+        window.location = "https://juliodeoliveira.github.io/portfolio";
+    }
+    else if (userInput.value == "Keven") {
+        window.location = "https://juliodeoliveira.github.io/portfolio-keven/portfolio";
+    }
+
+    else if (userInput.value == "Eduardo") {
+        window.location = "https://eduardo21072005.github.io/portifolio/startbootstrap-resume-gh-pages/index.html";
+
+    }
+
+    else if (userInput.value == "Cauã") {
+        window.location = "https://cauam1203.netlify.app/";
+
+    } else {
+        userResult.innerHTML = "";
+    }
+}
